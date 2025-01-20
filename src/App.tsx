2e9eaf034f914a9ead46a8b28e199e35035d89bc
Hannah Redler <hannahjredler@gmail.com>
2025-01-20 18:07:45 +0000

    Cleanup, add tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-600">ZK Powered Verifiable Calculator</h1>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
