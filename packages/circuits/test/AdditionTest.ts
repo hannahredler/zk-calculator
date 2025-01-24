19b23ae327dc8370d0f5e9e3b4bb72ca3251aa2b
Hannah Redler <hannahjredler@gmail.com>
2025-01-24 04:55:40 -0800

    Workspaces refactor

---
+++ packages/circuits/test/AdditionTest.ts
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+// import { GrothV}
+
+describe("Addition", () => {
+  let Verifier;
+  beforeEach(() => {
+    ethers.getContractFactory(AdditionV);
+  });
+});
