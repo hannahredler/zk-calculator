22a8ad1794faf0d90941692a7612d83025183864
Hannah Redler <hannahjredler@gmail.com>
2025-01-28 23:06:34 -0800

    Finish powers of tau setup

--- packages/circuits/scripts/setup-tau.sh
+++ packages/circuits/scripts/setup-tau.sh
@@ -7,7 +7,10 @@ else
     mkdir -p ./trusted-setup
 fi
 
+# start a new ceremony
 snarkjs powersoftau new bn128 12 ./trusted-setup/my_powersOfTau.ptau
+
+# contribute to the ceremony
 snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau.ptau ./trusted-setup/my_powersOfTau_1.ptau --name="First Contributor"
 snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_1.ptau ./trusted-setup/my_powersOfTau_2.ptau --name="Second Contributor"
 snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_2.ptau ./trusted-setup/my_powersOfTau_3.ptau --name="Third Contributor"
@@ -20,7 +23,17 @@ snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_8.ptau ./trusted-s
 snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_9.ptau ./trusted-setup/my_powersOfTau_10.ptau --name="Tenth Contributor"
 snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_10.ptau ./trusted-setup/my_powersOfTau_11.ptau --name="Eleventh Contributor"
 snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_11.ptau ./trusted-setup/my_powersOfTau_12.ptau --name="Twelfth Contributor"
-snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_12.ptau ./trusted-setup/my_powersOfTau_final.ptau --name="Final Contributor"
+
+# Apply a random beacon (normally something can't be known ahead of time) (see here for more: https://github.com/iden3/snarkjs?tab=readme-ov-file#6-apply-a-random-beacon)
+snarkjs powersoftau beacon ./trusted-setup/my_powersOfTau_12.ptau ./trusted-setup/my_powersOfTau_beacon.ptau 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Final Beacon"
+
+# verify the ceremony
+snarkjs powersoftau verify ./trusted-setup/my_powersOfTau_beacon.ptau
+
+# prepare phase 2 of the ceremony
+snarkjs powersoftau prepare phase2 ./trusted-setup/my_powersOfTau_beacon.ptau ./trusted-setup/my_powersOfTau_final.ptau -v
+
+# verify final output
 snarkjs powersoftau verify ./trusted-setup/my_powersOfTau_final.ptau
 
 
