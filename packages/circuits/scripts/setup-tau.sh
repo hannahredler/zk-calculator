92104eef8c7892445cbf422a7365e330807e4a34
Hannah Redler <hannahjredler@gmail.com>
2025-02-03 17:27:37 +0100

    Modify powers of tau folder to be within the circuits package

--- packages/circuits/scripts/setup-tau.sh
+++ packages/circuits/scripts/setup-tau.sh
@@ -1,37 +1,41 @@
-# Check if trusted-setup directory exists
-if [ -d "./trusted-setup" ]; then
+# Define the circuits directory path
+CIRCUITS_DIR="$(dirname "$(dirname "$0")")"
+TRUSTED_SETUP_DIR="$CIRCUITS_DIR/trusted-setup"
+
+# Check if trusted-setup directory exists in circuits folder
+if [ -d "$TRUSTED_SETUP_DIR" ]; then
     # Directory exists, clean its contents
-    rm -rf ./trusted-setup/*
+    rm -rf "$TRUSTED_SETUP_DIR"/*
 else
     # Directory doesn't exist, create it
-    mkdir -p ./trusted-setup
+    mkdir -p "$TRUSTED_SETUP_DIR"
 fi
 
 # start a new ceremony
-snarkjs powersoftau new bn128 12 ./trusted-setup/my_powersOfTau.ptau
+snarkjs powersoftau new bn128 12 "$TRUSTED_SETUP_DIR/my_powersOfTau.ptau"
 
 # contribute to the ceremony
-snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau.ptau ./trusted-setup/my_powersOfTau_1.ptau --name="First Contributor"
-snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_1.ptau ./trusted-setup/my_powersOfTau_2.ptau --name="Second Contributor"
-snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_2.ptau ./trusted-setup/my_powersOfTau_3.ptau --name="Third Contributor"
-snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_3.ptau ./trusted-setup/my_powersOfTau_4.ptau --name="Fourth Contributor"
-snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_4.ptau ./trusted-setup/my_powersOfTau_5.ptau --name="Fifth Contributor"
-snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_5.ptau ./trusted-setup/my_powersOfTau_6.ptau --name="Sixth Contributor"
-snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_6.ptau ./trusted-setup/my_powersOfTau_7.ptau --name="Seventh Contributor"
-snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_7.ptau ./trusted-setup/my_powersOfTau_8.ptau --name="Eighth Contributor"
-snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_8.ptau ./trusted-setup/my_powersOfTau_9.ptau --name="Ninth Contributor"
-snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_9.ptau ./trusted-setup/my_powersOfTau_10.ptau --name="Tenth Contributor"
-snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_10.ptau ./trusted-setup/my_powersOfTau_11.ptau --name="Eleventh Contributor"
-snarkjs powersoftau contribute ./trusted-setup/my_powersOfTau_11.ptau ./trusted-setup/my_powersOfTau_12.ptau --name="Twelfth Contributor"
+snarkjs powersoftau contribute "$TRUSTED_SETUP_DIR/my_powersOfTau.ptau" "$TRUSTED_SETUP_DIR/my_powersOfTau_1.ptau" --name="First Contributor"
+snarkjs powersoftau contribute "$TRUSTED_SETUP_DIR/my_powersOfTau_1.ptau" "$TRUSTED_SETUP_DIR/my_powersOfTau_2.ptau" --name="Second Contributor"
+snarkjs powersoftau contribute "$TRUSTED_SETUP_DIR/my_powersOfTau_2.ptau" "$TRUSTED_SETUP_DIR/my_powersOfTau_3.ptau" --name="Third Contributor"
+snarkjs powersoftau contribute "$TRUSTED_SETUP_DIR/my_powersOfTau_3.ptau" "$TRUSTED_SETUP_DIR/my_powersOfTau_4.ptau" --name="Fourth Contributor"
+snarkjs powersoftau contribute "$TRUSTED_SETUP_DIR/my_powersOfTau_4.ptau" "$TRUSTED_SETUP_DIR/my_powersOfTau_5.ptau" --name="Fifth Contributor"
+snarkjs powersoftau contribute "$TRUSTED_SETUP_DIR/my_powersOfTau_5.ptau" "$TRUSTED_SETUP_DIR/my_powersOfTau_6.ptau" --name="Sixth Contributor"
+snarkjs powersoftau contribute "$TRUSTED_SETUP_DIR/my_powersOfTau_6.ptau" "$TRUSTED_SETUP_DIR/my_powersOfTau_7.ptau" --name="Seventh Contributor"
+snarkjs powersoftau contribute "$TRUSTED_SETUP_DIR/my_powersOfTau_7.ptau" "$TRUSTED_SETUP_DIR/my_powersOfTau_8.ptau" --name="Eighth Contributor"
+snarkjs powersoftau contribute "$TRUSTED_SETUP_DIR/my_powersOfTau_8.ptau" "$TRUSTED_SETUP_DIR/my_powersOfTau_9.ptau" --name="Ninth Contributor"
+snarkjs powersoftau contribute "$TRUSTED_SETUP_DIR/my_powersOfTau_9.ptau" "$TRUSTED_SETUP_DIR/my_powersOfTau_10.ptau" --name="Tenth Contributor"
+snarkjs powersoftau contribute "$TRUSTED_SETUP_DIR/my_powersOfTau_10.ptau" "$TRUSTED_SETUP_DIR/my_powersOfTau_11.ptau" --name="Eleventh Contributor"
+snarkjs powersoftau contribute "$TRUSTED_SETUP_DIR/my_powersOfTau_11.ptau" "$TRUSTED_SETUP_DIR/my_powersOfTau_12.ptau" --name="Twelfth Contributor"
 
-# Apply a random beacon (normally something can't be known ahead of time) (see here for more: https://github.com/iden3/snarkjs?tab=readme-ov-file#6-apply-a-random-beacon)
-snarkjs powersoftau beacon ./trusted-setup/my_powersOfTau_12.ptau ./trusted-setup/my_powersOfTau_beacon.ptau 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Final Beacon"
+# Apply a random beacon
+snarkjs powersoftau beacon "$TRUSTED_SETUP_DIR/my_powersOfTau_12.ptau" "$TRUSTED_SETUP_DIR/my_powersOfTau_beacon.ptau" 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Final Beacon"
 
 # verify the ceremony
-snarkjs powersoftau verify ./trusted-setup/my_powersOfTau_beacon.ptau
+snarkjs powersoftau verify "$TRUSTED_SETUP_DIR/my_powersOfTau_beacon.ptau"
 
 # prepare phase 2 of the ceremony
-snarkjs powersoftau prepare phase2 ./trusted-setup/my_powersOfTau_beacon.ptau ./trusted-setup/my_powersOfTau_final.ptau -v
+snarkjs powersoftau prepare phase2 "$TRUSTED_SETUP_DIR/my_powersOfTau_beacon.ptau" "$TRUSTED_SETUP_DIR/my_powersOfTau_final.ptau" -v
 
 # verify final output
-snarkjs powersoftau verify ./trusted-setup/my_powersOfTau_final.ptau
+snarkjs powersoftau verify "$TRUSTED_SETUP_DIR/my_powersOfTau_final.ptau"
